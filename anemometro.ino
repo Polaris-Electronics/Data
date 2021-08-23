eb08485b448f9ef0526171fdadada75d38b296a9
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2021-08-23 15:24:17 -0600

    Add files via upload

--- anemometro.ino
+++ anemometro.ino
@@ -1,9 +1,12 @@
+const float pi = 3.14;
 int contador = 0;
 bool dato = LOW;
 bool datoAnterior = LOW;
 float tiempoAnterior = 0;
 float tiempo = 0;
 float diferencia = 0;
+float velocidad = 0;
+float w = 0;
 
 void setup() {
 pinMode(2,INPUT);
@@ -17,8 +20,13 @@ void loop() {
   tiempo=millis();
   contador++;
   diferencia = tiempo-tiempoAnterior;
-  Serial.println(diferencia);
   tiempoAnterior=tiempo;
+  w=(2*pi)/(diferencia/1000);
+  velocidad=w*0.152; // dimensiones anemometro 
+  Serial.println("T");
+  Serial.println(diferencia);
+  Serial.println("Velocidad");
+  Serial.println(velocidad);
   
  }
  datoAnterior = dato;
